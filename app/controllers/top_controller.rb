53f2d8b08e7425d8538a7f850a0bf50cf49cd43e
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2021-07-13 15:48:21 +0900

    rails db:migrate:down (dummy file : create)
    rails db:migrate:status　で確認。
    no fileがあったため、rails db:rollbackが行えず
    修正が行えない。（DBのカラム情報）
    
    touch db/migrate/20210709035017_hoge.rb
    こちらで上記 no fileに名前をつけてダミーファイルにした。
    その後、
    rails db:migrate:down VERSION=20210709035017
    こちらで、ダミーファイルがdown状態になった。
    
    ファイルを消す準備が整った。

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,12 @@ class TopController < ApplicationController
   def index
     render "layouts/index.html.haml"
   end
-  def destroy
-    
-  end
+
+  # def after_sign_in_path_for(resource)
+  #   root_path
+  # end
+
+  # def after_sign_out_path_for(resource)
+  #   redirect_to root_path
+  # end
 end
