816cf9ce9f19ebc5f2554b888b5da03ac37b6287
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2021-07-16 16:25:11 +0900

    top_controller.rb　　histricalfigures_controller.rb
    １：必要の無い・無駄だった記述の消去
    
    ２：histricalfigures_controller.rbに関しては
    歴史上の人物のプロフィール画面で
    使う予定で作成。

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,12 +2,4 @@ class TopController < ApplicationController
   def index
     render "layouts/index.html.haml"
   end
-
-  # def after_sign_in_path_for(resource)
-  #   root_path
-  # end
-
-  # def after_sign_out_path_for(resource)
-  #   redirect_to root_path
-  # end
 end
