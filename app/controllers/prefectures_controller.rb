93dfa85b11d3cf643e9f4d80ecc0bac0eacf004b
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2021-07-19 12:08:09 +0900

    @prefectureName = prefecture[:name]
    idに対応した都道府県の名前を取り出す。

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -2,6 +2,9 @@ class PrefecturesController < ApplicationController
 
   def show
     render "/prefectures/new.html.haml"
+    prefecture = Prefecture.find(params[:id])
+    @prefectureName = prefecture[:name]
   end
 
 end
+
