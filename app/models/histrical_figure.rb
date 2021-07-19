3e51ed888f29d7f1a159b19e92280f712a7b06ca
yamakamineyu <yamamoto-luffysiro@docomo.ne.jp>
2021-07-19 12:51:01 +0900

    _create_histrical_figures.rb
    _create_histrical_figures.rb
    偉人に関して、新規登録を行えるように
    DB上のカラムを加えた。
    
    rails db:migrate
    も行った。sequel Proでカラムの項目が増えている
    ことを確認した。
    
    histrical_figure.rb
    <＃アソシエーション>を付け加えただけ。
    （コメントアウトの記述）

--- app/models/histrical_figure.rb
+++ app/models/histrical_figure.rb
@@ -1,6 +1,8 @@
 class HistricalFigure < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
+
+  # アソシエーション
   belongs_to_active_hash :prefecture
-  
+
 end
--- db/migrate/20210716053116_create_histrical_figures.rb
+++ db/migrate/20210716053116_create_histrical_figures.rb
@@ -1,8 +1,14 @@
 class CreateHistricalFigures < ActiveRecord::Migration[5.0]
   def change
     create_table :histrical_figures do |t|
-      t.string :name
-      t.integer :prefecture_id
+      t.string   :name,            null: false
+      t.string   :last_name,       null: false
+      t.string   :first_name,      null: false
+      t.string   :last_name_kana,  null: false
+      t.string   :first_name_kana, null: false
+      t.string   :nickname
+      t.integer  :prefecture_id,   null: false
+      t.text     :chronology
 
       t.timestamps
     end
